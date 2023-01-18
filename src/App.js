6d3f760f8f498d038f14bfaa8d44109caac83d43
Grenky <demosoft62@gmail.com>
2023-01-18 14:50:19 +0200

    update2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import './App.css';
 import Nav from './components/Nav';
-// import { Routes, Route, Link} from 'react-router-dom';
-// import About from './components/About';
-// import Characters from './components/Characters';
-// import Something from './components/Something';
+import { Routes, Route} from 'react-router-dom';
+import About from './components/About';
+import Characters from './components/Characters';
+import Something from './components/Something';
 
 function App() {
   
   return (
     
     <div className="App"> 
-      <div className='wrapper'>
-        <Nav />   
-      
-     </div>
-     
-    </div>
-    
-    
-    
+        <Nav /> 
+        <Routes>
+            <Route path="characters" element={<Characters />} />
+            <Route path="something" element={<Something />} />
+            <Route path="about" element={<About />} />
+        </Routes>    
+     </div>    
   );
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,7 @@
-import React from "react";
- import { Routes, Route, Link} from 'react-router-dom';
- import About from './About';
- import Characters from './Characters';
- import Something from './Something';
+ import React from "react";
+ import {Link} from 'react-router-dom';
+
+ import './Nav.css';
 
 function Nav() {
     return(
@@ -12,12 +11,7 @@ function Nav() {
                 <Link className='nav-link' to='Characters'>characters</Link>
                 <Link className='nav-link' to='Something'>something</Link>
                 <Link className='nav-link' to='About'>about</Link>
-            </nav> 
-        <Routes>
-            <Route path="characters" element={<Characters />} />
-            <Route path="something" element={<Something />} />
-            <Route path="about" element={<About />} />
-        </Routes> 
+            </nav>   
         </div>
     )
 }
