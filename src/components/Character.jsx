09ba384405fe796934d1bb6eb410522a13cff01b
Grenky <demosoft62@gmail.com>
2023-01-27 19:10:57 +0200

    update6

---
+++ src/components/Character.jsx
@@ -0,0 +1,29 @@
+import React  from "react";
+import '../styles/Characters.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function Character({characterId}) {
+
+  const url = `https://rickandmortyapi.com/api/character/${characterId}`;
+  const [character, setCharacter] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then(response => { 
+        setCharacter(response.data)});
+  }, []);
+
+    return(
+        <div className="char_wrapper">
+            <h1 className="char_title">all characters</h1>
+            <div className="chara_box"> 
+              <p>{character.name}</p>
+              <img src={character.image} alt={character.name} />
+            </div>
+        </div>
+    )
+}
+
+export default Character;
--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,37 +1,19 @@
 import React  from "react";
 import '../styles/Characters.css';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import Character from "./Character";
 
 function Characters() {
-
-  const url = "https://rickandmortyapi.com/api/character/271";
-
-  // axios.get(url).then((res) => {
-  //   console.log(res.data.image)
-  // })
-
-  const [vagina, setVagina] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get(url)
-      .then(data => {
-        setVagina(data.data)});
-  }, []);
-
-    return(
-        <div className="char_wrapper">
-            <h1 className="char_title">all characters</h1>
-            <div className="chara_box">
-            {vagina.map(vaginas => {
-              return(
-                <p>{vaginas.image}</p>
-              );
-            })}
+  
+      return(
+          <div className="char_wrapper">
+            <Character characterId={271}/>
+            <Character characterId={192}/>
+            <Character characterId={594}/>
+            <Character characterId={328}/>
+            <Character characterId={529}/>
+            <Character characterId={679}/>
             </div>
-        </div>
-    )
-}
-
-export default Characters;
+      )
+  }
+  
+  export default Characters;
